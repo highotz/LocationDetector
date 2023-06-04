ce46f8bd60bc0247d1d9a03495d6b7d10703316b
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2023-06-03 23:45:46 -0300

    Atualizando Nuget Packages e adicionando IpTranslationController

---
+++ LocationDetector.API/Controllers/IpTranslationController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationDetector.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IpTranslationController : ControllerBase
+{
+    private readonly ILogger<IpTranslationController> _logger;
+
+    public IpTranslationController(ILogger<IpTranslationController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProcessIps()
+    {
+        // Check if a file was sent in the request
+        if (Request.Form.Files.Count == 0)
+        {
+            return BadRequest("No files have been uploaded.");
+        }
+
+        var arquivo = Request.Form.Files[0];
+
+        // Check the file type (CSV or JSON)
+        if (arquivo.FileName.EndsWith(".csv"))
+        {
+            // Process CSV File
+            using var reader = new StreamReader(arquivo.OpenReadStream());
+            var content = await reader.ReadToEndAsync();
+
+            return Ok("Arquivo CSV processado com sucesso.");
+        }
+        else if (arquivo.FileName.EndsWith(".json"))
+        {
+            // Process JSON file
+
+            using var reader = new StreamReader(arquivo.OpenReadStream());
+            var content = await reader.ReadToEndAsync();
+
+            return Ok("JSON file successfully processed.");
+        }
+        else
+        {
+            return BadRequest("Invalid file type. Only CSV or JSON files are allowed.");
+        }
+    }
+}
