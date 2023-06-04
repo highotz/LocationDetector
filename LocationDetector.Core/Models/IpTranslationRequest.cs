d3ab020756ce52f5c5bf5831cd56e23a4e0da2d6
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2023-06-04 05:17:15 -0300

    Objetos de entrada e retorno

---
+++ LocationDetector.Core/Models/IpTranslationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocationDetector.Core.Models
+{
+    public class IpTranslationRequest
+    {
+        public TimestampAttribute timestamp { get; set; }
+        public Guid id { get; set; }
+        public string ip { get; set; }
+    }
+}
---
+++ LocationDetector.Core/Models/IpTranslationResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationDetector.Core.Models
+{
+    internal class IpTranslationResponse : IpTranslationRequest
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string country { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+    }
+}
